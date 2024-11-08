c6a329590905af9e217626c484b33ee9438fd70d
Zuanid-Sutlan <zunaidsultan786@gmail.com>
2024-11-08 09:12:50 +0500

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/BakingViewModel.kt
@@ -0,0 +1,55 @@
+package com.example.myapplication
+
+import android.graphics.Bitmap
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.ai.client.generativeai.GenerativeModel
+import com.google.ai.client.generativeai.type.content
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import java.io.ByteArrayOutputStream
+
+class BakingViewModel : ViewModel() {
+    private val _uiState: MutableStateFlow<UiState> =
+        MutableStateFlow(UiState.Initial)
+    val uiState: StateFlow<UiState> =
+        _uiState.asStateFlow()
+
+    private val generativeModel = GenerativeModel(
+        modelName = "gemini-1.5-flash", // "gemini-pro-vision",
+        apiKey = BuildConfig.apiKey,
+    )
+
+    fun sendPrompt(
+        bitmap: Bitmap,
+        prompt: String
+    ) {
+        _uiState.value = UiState.Loading
+
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+
+                // Convert Bitmap to byte array
+//                val stream = ByteArrayOutputStream()
+//                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream)
+//                val byteArray = stream.toByteArray()
+
+                val response = generativeModel.generateContent(
+                    content {
+                        image(bitmap)
+                        text("")
+                    }
+                )
+
+                response.text?.let { outputContent ->
+                    _uiState.value = UiState.Success(outputContent)
+                }
+            } catch (e: Exception) {
+                _uiState.value = UiState.Error(e.localizedMessage ?: "")
+            }
+        }
+    }
+}
